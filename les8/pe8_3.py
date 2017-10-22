6acf5075c3023af9c99709b529c7e23a75b387ab
Frederiquee <frederique.dorrestein@student.hu.nl>
2017-10-22 22:41:37 +0200

    opdrachten van les 2 & 3

--- les8/pe8_3.py
+++ les8/pe8_3.py
@@ -1,1 +1,9 @@
-#def code(invoerstring):
+def code(invoerstring):
+    resultaat = ''
+
+    for resultaat in invoerstring:
+        resultaat += chr(ord(resultaat)+3)
+
+    return resultaat
+
+print(code(input('Geef uw naam, beginstation en eindstation: ')))
